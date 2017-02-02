e28601cc901505fbda35a483db0e45f5d7afde1d
michel <micheldaniel2@hotmail.com>
2017-02-02 16:26:49 +0100

    update installer
    installer.js zo gemaakt dat functie uit main.js kunnen worden
    aangeroepen

--- installer.js
+++ installer.js
@@ -1,16 +1,27 @@
 //load modules
 var fs = require('fs');
+var ConsoleColor = require('./ConsoleColor.js');
 
-//save file location
-fs.writeFile("./config/fileLocation.txt", JSON.stringify({
-    file: __dirname,
-    fileLocation: __dirname
-}));
+//fileLocationInstall
+function fileLocationInstall(){
+    
+    //save file location
+    fs.writeFile("./config/fileLocation.txt", JSON.stringify({
+        file: __dirname,
+        fileLocation: __dirname
+    }));
 
-//dir path
-var dir = __dirname+'/temp';
+    //dir path
+    var dir = __dirname+'/temp';
 
-//kijk of folder bestaat
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+    //kijk of folder bestaat
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
 }
+
+/* ==================== GETTER ====================  */
+exports.fileLocationInstall = function(){
+    fileLocationInstall();
+    console.log(ConsoleColor.log()+"Filelocation.txt aangemaakt.");
+};
