4041d11054387f89632f3a1638e52a6164eaf308
michel <micheldaniel2@hotmail.com>
2017-02-05 02:43:24 +0100

    kleine bug fix
    kleine foutjes verholpen.

--- ConsoleColor.js
+++ ConsoleColor.js
@@ -18,35 +18,35 @@ function date(){
     var date = new Date();
     
     //maand
-    if (date.getMonth() < 9){
+    if (date.getMonth() < 10){
         var maand = ""+date.getMonth()+1;
     } else {
         var maand = (date.getMonth())+11;
     }
     
     //dag
-    if (date.getDate() < 9){
+    if (date.getDate() < 10){
         var dag = "0"+date.getDate();
     } else {
         var dag = date.getDate();
     }
     
     //uur
-    if (date.getHours()  < 9){
+    if (date.getHours()  < 10){
         var uur = "0"+date.getHours();
     } else {
         var uur = date.getHours();
     }
     
     //minut
-    if (date.getMinutes()  < 9){
+    if (date.getMinutes()  < 10){
         var minut = "0"+date.getMinutes();
     } else {
         var minut = date.getMinutes();
     }
     
     //sec
-    if (date.getSeconds()  < 9){
+    if (date.getSeconds()  < 10){
         var sec = "0"+date.getSeconds();
     } else {
         var sec = date.getSeconds();
--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
 //load modules
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 //router
 var Router = express.Router();
@@ -9,30 +10,21 @@ var Router = express.Router();
 Router.use(bodyParser.json());
 Router.use(bodyParser.urlencoded({ extended: true }));
 
-//load modules
-var updateBalance = require('./updateBalance.js');
+//krijg de filelocation
+var fileLocation = JSON.parse(fs.readFileSync('./config/fileLocation.txt')).fileLocation;
 
+//load modules
+var updateBalance = require(fileLocation+'/router/updateBalance.js');
+var GetIpAddress = require(fileLocation+'/scripts/IpAddress.js');
+
+//Router.post
 Router.post('/updatebalance', function(req, res){
     
     //req data
     var reqData = req.body;
-    var rawIp = req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress || 
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress;
-    
-    //kijk welke ip adres versie er is
-    console.log(rawIp.substring(0,7));
-    if (rawIp.substring(0, 7) == "::ffff:") {
-        
-        //het begin van ipv4 er waf slopen
-        var ip = rawIp.substring(7);
-        console.log(ip);
-    }
-    
     
     //load updatebalance function terugKoppeling
-    var terugKoppeling = updateBalance.updateBalance(JSON.stringify(reqData), ip);
+    var terugKoppeling = updateBalance.updateBalance(JSON.stringify(reqData), GetIpAddress.ipAddress(req));
     
     //kijk om te kijken of het succes vol is gelukt
     if(terugKoppeling == true){
