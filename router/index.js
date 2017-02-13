1f22a9632c1959e18e149ba725ac10ad619368c7
michel <micheldaniel2@hotmail.com>
2017-02-13 23:23:24 +0100

    update get balance.js
    update get balance

--- router/index.js
+++ router/index.js
@@ -40,82 +40,12 @@ MYSQLConnection.connect(function(err){
     }
 });
 
-//function
-Router.post('/', function(req, res){
-    
-    //vraag data op
-    console.log(req.body);
-    
-    //Maak een sql string
-    var ip = GetIpAddress.ipAddress(req);
-    console.log(ip)
-    
-    //voor sting uit
-    console.log("SELECT * FROM cryptoData.balance WHERE ip='"+ip+"';");
-    MYSQLConnection.query("SELECT * FROM cryptoData.balance WHERE ip='"+ip+"';", function (err, resulttwo) {
-        if (err) {
-            console.error(ConsoleColor.error()+"Probleem bij balance data op te vragen. Dit probleem is bij getBalance.js.");
-        } else {
-            res.send(resulttwo);
-        }
-    });
-});
+//post
+app.use('/updatebalance', require(fileLocation+'/router/updateBalance.js'));
 
-//orderssql query
-Router.post('/ordersSqlQuery', function(req, res){
-    
-    //verwerk alle data van het inkomende verkeer
-    var ip = GetIpAddress.ipAddress(req);
-    var tempData = req.body;
-    var settingOrdersBittrex = JSON.parse(tempData[0]).bittrex;
-    var settingOrdersPoloniex = JSON.parse(tempData[0]).poloniex;
-    
-    
-    //var sql
-    var sqlQuery = "SELECT * FROM orderLimiet";
-    var finalSqlQuery = "SELECT * FROM orderLimiet";
-    
-    //settingOrderBittrex
-    if(settingOrdersBittrex == true){
-        
-        if(sqlQuery == finalSqlQuery) {
-            sqlQuery += " WHERE handelsplaats='bittrex'";   
-            console.log(sqlQuery);
-        } else {
-            sqlQuery += " AND handelsplaats='bittrex";
-        }
-    };
-    
-    //settingOrderPoloniex
-    if(settingOrdersPoloniex == true){
-        
-        if(sqlQuery == finalSqlQuery) {
-            sqlQuery += " WHERE handelsplaats='poloniex'";
-        } else {
-            sqlQuery += " AND handelsplaats='poloniex";
-        }
-    };
-    
-    //mysql connection
-    MYSQLConnection.query(sqlQuery, function (error, results, fields) {
-        // error will be an Error if one occurred during the query 
-        // results will contain the results of the query 
-        // fields will contain information about the returned results fields (if any) 
-        if(error){
-            console.log(error);
-        } else {
-            console.log("results "+JSON.stringify(results));
-            if(sqlQuery != finalSqlQuery){
-                res.send(results[0]);
-            }
-        }
-    });
-});
-
-
-
-
-
+//getter
+app.use('/getorder', require(fileLocation+'/router/GetOrders.js'));
+app.use('/getbalance', require(fileLocation+'/router/GetBalance.js'));
 
 //export Router
-module.exports = Router;
+module.exports = app;
