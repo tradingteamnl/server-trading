0d65946f05db958927a518599fb25a7e5f5e3a54
michel <micheldaniel2@hotmail.com>
2017-02-18 00:24:35 +0100

    configgetter
    onzinnige regels weer verwijderd

--- configGetter.js
+++ configGetter.js
@@ -25,7 +25,7 @@ exports.dag = function(){
 /* ====================== TIJD ======================  */
 exports.tijd = function (){
     return tijd.tijd;
-}
+};
 
 /* ================== FILELOCATION ================== */
 exports.fileLocation = function(){
@@ -33,39 +33,9 @@ exports.fileLocation = function(){
 };
 
 /* ================== MYSQL ================== */
-/* ============== MYSQL HOST ============== */
-exports.mysqlHost = function(){
-    return config.mysql.host;
-};
-
-/* ============== MYSQL USER ============== */
-exports.mysqlUser = function(){
-    return config.mysql.user;
-};
-
-/* ============== MYSQL PASSWORD ============== */
-exports.mysqlPassword = function(){
-    return config.mysql.password;
-};
-
 /* ============== MYSQL DBNAME ============== */
 exports.mysqlDBName = function(){
     return config.mysql.DBName;
-};
-
-/* ======= MYSQL DEVELOPER DATABSE ======= */
-exports.mysqlDeveloperDatabaseHost = function(){
-    return config.mysqlDeveloperDatabase.host;
-};
-
-/* ============== MYSQL USER ============== */
-exports.mysqlDeveloperDatabaseUser = function(){
-    return config.mysqlDeveloperDatabase.user;
-};
-
-/* ============== MYSQL PASSWORD ============== */
-exports.mysqlDeveloperDatabasePassword = function(){
-    return config.mysqlDeveloperDatabase.password;
 };
 
 /* ============== MYSQL DBNAME ============== */
