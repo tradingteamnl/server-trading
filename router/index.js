0da722b0d526a4b3b58d88437a80df94c9b2fed0
michel <micheldaniel2@hotmail.com>
2017-02-17 09:43:47 +0100

    verwijderd code
    verwijderd onzinnige code

--- router/index.js
+++ router/index.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var mysql = require('mysql');
 
 var app = express();
 
@@ -16,32 +15,9 @@ Router.use(bodyParser.urlencoded({ extended: true }));
 //krijg de filelocation
 var fileLocation = JSON.parse(fs.readFileSync('./config/fileLocation.txt')).fileLocation;
 
-//load modules
-var GetIpAddress = require(fileLocation+'/scripts/IpAddress.js');
-var ConsoleColor = require(fileLocation+'/ConsoleColor.js');
-
-//laat config
-var config = JSON.parse(fs.readFileSync(fileLocation+'/config.json'));
-
-//connection
-var MYSQLConnection = mysql.createConnection({
-    host     : config.mysql.host,
-    user     : config.mysql.user,
-    password : config.mysql.password,
-    database : config.mysql.DBName
-});
-
-//connectie maken met mysql
-MYSQLConnection.connect(function(err){
-    if(err){
-        console.error(ConsoleColor.error()+err);
-    } else {
-        console.log(ConsoleColor.log()+"Connectie met mysql.");
-    }
-});
-
 //post
 app.use('/updatebalance', require(fileLocation+'/router/updateBalance.js'));
+app.use('/getbesteprijs', require(fileLocation+'/GetBestePrijs.js'));
 
 //getter
 app.use('/getorder', require(fileLocation+'/router/GetOrders.js'));
